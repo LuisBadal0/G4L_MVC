4c540603ab43d6d4c5d92af2ae207c71f9cbee6f
LuisBadal0 <luismbbadalo@outlook.com>
2023-11-16 19:05:02 +0000

    Show Categories Values in a table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,15 @@ namespace BulkyWeb.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "RPG", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Action", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Sports", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Horror", DisplayOrder = 3 }
+                );
+        }
     }
 }
