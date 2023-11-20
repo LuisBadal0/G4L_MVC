f7b576e28eacc86c96f527199711e013350b8e03
LuisBadal0 <luismbbadalo@outlook.com>
2023-11-20 23:29:08 +0000

    Added New table with Ajax Calls

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'productName', "width": "15%" },
+            { data: 'developer', "width": "10%" },
+            { data: 'publisher', "width": "10%" },
+            { data: 'description', "width": "25%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'category.name', "width": "15%" },
+        ]
+    });
+}
+
