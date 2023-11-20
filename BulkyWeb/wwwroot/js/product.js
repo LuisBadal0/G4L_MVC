c0e1170ab5d474cfc203fda09d4510821491b70a
LuisBadal0 <luismbbadalo@outlook.com>
2023-11-20 23:42:55 +0000

    API point Edit

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -12,6 +12,16 @@ function loadDataTable() {
             { data: 'description', "width": "25%" },
             { data: 'listPrice', "width": "10%" },
             { data: 'category.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href = "/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square" ></i ></a>
+                        <a href = "" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill" ></i ></a>
+                    </div >`
+                },
+                "width": "10%"
+            },
         ]
     });
 }
