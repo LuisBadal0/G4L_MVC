fb399b273ee93b1cde27d38777722c5c90050493
LuisBadal0 <luismbbadalo@outlook.com>
2023-11-21 12:45:08 +0000

    Add more collumn in IdentityUser

--- Bulky.DataAccess/Migrations/20231121124419_ExtendIdentityUser.Designer.cs
+++ Bulky.DataAccess/Migrations/20231121124419_ExtendIdentityUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StoreGWeb.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using StoreGWeb.DataAccess.Data;
 namespace StoreGWeb.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231121124419_ExtendIdentityUser")]
+    partial class ExtendIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
