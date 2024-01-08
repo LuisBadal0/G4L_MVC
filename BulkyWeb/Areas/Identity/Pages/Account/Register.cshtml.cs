ae38eb27eea2394cc240289919c9c203282dee16
LuisBadal0 <luismbbadalo@outlook.com>
2024-01-08 17:19:15 +0000

    Company Dropdown Registering Account

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -20,6 +20,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using StoreG.DataAccess.Repository.IRepository;
 using StoreG.Models;
 using StoreG.Utility;
 
@@ -34,6 +35,7 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,8 +43,10 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
+
             _roleManager = roleManager;
             _userManager = userManager;
             _userStore = userStore;
@@ -50,6 +54,7 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -118,6 +123,10 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
             public string? StreetAddress { get; set; }
 
             public string? PhoneNumber { get; set; }
+            public int? CompanyId { get; set; }
+
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
 
 
@@ -140,6 +149,11 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
                     Text = i,
                     Value = i
                 }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
             };
 
             ReturnUrl = returnUrl;
@@ -159,7 +173,7 @@ namespace StoreGWeb.Areas.Identity.Pages.Account
                 user.StreetAddress = Input.StreetAddress;
                 user.City = Input.City;
                 user.Name = Input.Name;
-                user.PostalCode = Input.PostalCode; 
+                user.PostalCode = Input.PostalCode;
                 user.Country = Input.Country;
                 user.PhoneNumber = Input.PhoneNumber;
                 var result = await _userManager.CreateAsync(user, Input.Password);
