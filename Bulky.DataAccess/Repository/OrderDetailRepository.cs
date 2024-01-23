28dff92adbf4ab99e0f3e5415b20c49dbf095913
LuisBadal0 <luismbbadalo@outlook.com>
2024-01-23 23:32:16 +0000

    Add Order Header and Detail Repository

---
+++ Bulky.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using StoreG.DataAccess.Repository.IRepository;
+using StoreG.Models;
+using StoreGWeb.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreG.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<Category>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetail.Update(obj);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,25 @@
+﻿using StoreG.DataAccess.Repository.IRepository;
+using StoreG.Models;
+using StoreGWeb.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreG.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderDetail>, IOrderHeaderRepository 
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Category obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
