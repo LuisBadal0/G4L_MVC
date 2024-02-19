7a814c313200510425457ac56074f49dbf029f29
LuisBadal0 <luismbbadalo@outlook.com>
2024-02-19 16:37:32 +0000

    All order can be seen by admin and employee only

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using StoreG.Models;
 using StoreG.Models.ViewModels;
 using StoreG.Utility;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace StoreGWeb.Areas.Admin.Controllers
 {
@@ -73,7 +74,18 @@ namespace StoreGWeb.Areas.Admin.Controllers
         public IActionResult GetAll(string status)
         {
 
-            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders;
+
+            if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee)){
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
 
             switch (status)
             {
