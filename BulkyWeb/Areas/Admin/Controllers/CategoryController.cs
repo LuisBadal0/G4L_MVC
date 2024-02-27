996add61266013c5bbc75c20028c8f0e945edefa
LuisBadal0 <luismbbadalo@outlook.com>
2024-02-27 15:26:31 +0000

    Fix Authorization commented

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ using StoreG.Utility;
 namespace StoreGWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    //[Authorize(Roles = SD.Role_Admin)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class CategoryController : Controller
     {
         
