d2169f56dcd4a5d2a33f5f534d857f71d676b253
LuisBadal0 <luismbbadalo@outlook.com>
2024-04-18 14:28:22 +0100

    Create user list management

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using StoreG.DataAccess.Repository.IRepository;
+using StoreG.Models;
+using StoreGWeb.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using StoreG.Models.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
+using StoreG.Utility;
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreGWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            //Get values from db to a variable
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll(int id)
+        {
+
+            List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            foreach (var user in objUserList)
+            {
+                if(user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
+            return Json(new { data = objUserList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            
+            return Json(new { success = true, message = "Deleted successfully" });
+        }
+        #endregion
+    }
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,28 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: 'name', className: "text-bg-dark", "width": "20%" },
+            { data: 'email', className: "text-bg-dark", "width": "15%" },
+            { data: 'company.name', className: "text-bg-dark", "width": "25%" },
+            { data: '', className: "text-bg-dark", "width": "5%" },
+            { data: 'phoneNumber', className: "text-bg-dark", "width": "15%" },
+            {
+                data: 'id', className: "text-bg-dark",
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href = "/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square" ></i ></a>
+                        <a onClick=Delete('/admin/company/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill" ></i ></a>
+                    </div >`
+                },
+                "width": "10%"
+            },
+        ]
+    });
+}
